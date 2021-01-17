c536355ca0bd9dda9e8e4d8340613bcdb622ffbb
judeakinwale <judeakinwale@gmail.com>
2021-01-17 14:46:39 +0100

    Add database
    fix some directory bugs

--- settings/production.py
+++ settings/production.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -117,12 +117,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_ROOT = BASE_DIR / 'meranda/static'
+STATIC_ROOT = BASE_DIR / 'static'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    BASE_DIR / 'meranda/meranda/static',
+    BASE_DIR / 'meranda/static',
 ]
 
 # Media files
-MEDIA_ROOT = BASE_DIR / 'meranda/media'
+MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/'
