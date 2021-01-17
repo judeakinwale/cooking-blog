1cca149b79cdcf461f2bc295a832c1f0b8e2c319
judeakinwale <judeakinwale@gmail.com>
2021-01-17 16:17:12 +0100

    Fix allowed hosts
    allow localhost to still work

--- settings/production.py
+++ settings/production.py
@@ -25,7 +25,7 @@ SECRET_KEY = ''
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [https://a-cooking-blog.herokuapp.com/]
+ALLOWED_HOSTS = ['https://a-cooking-blog.herokuapp.com/', '.localhost', '127.0.0.1', '[::1]']
 
 
 # Application definition
